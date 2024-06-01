b4409bd0554cbbb6d0f873372685899158b417c5
SAKSHI-KOUNDAL <168677390+SAKSHI-KOUNDAL@users.noreply.github.com>
2024-06-01 18:25:26 +0530

    Create js2.js

---
+++ js2.js
@@ -0,0 +1,43 @@
+//REPLACE
+//let str="I am sakshi koundal";
+//console.log(str.replace("i","my"));
+
+//EXAMPLE
+/*var city="delhi";
+console.log(city);
+{
+    let city="mumbai";
+
+
+}*/
+/*let x=4;
+if(x=== 7){
+console.log(x);
+}
+
+else if(x<=5)
+{
+    console.log(x+"bdhdh");
+}
+
+
+else if(x<=6)
+{
+    console.log(x);
+}
+else{
+    console.log("default");
+}*/
+/*let temp=34;
+if(temp<20){
+    console.log("It is very cold out ")
+}*/
+/*var flag= flase;
+if(flag)
+{
+    console.log("sakshi");
+}
+else 
+{
+    console.log("koundal");
+}**/
