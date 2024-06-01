c55163f33d94f9cb5af72f022a10b62826af27d0
SAKSHI-KOUNDAL <168677390+SAKSHI-KOUNDAL@users.noreply.github.com>
2024-06-01 18:30:12 +0530

    Create javas4.js

---
+++ javas4.js
@@ -0,0 +1,38 @@
+//functions
+
+       //sayhello("sakshi,hpurc khaniyara");
+      /*function sum(a,b,c){
+        return a+b+c;
+       }
+       console.log(sum(4,5,6));*/
+
+       //function sum(a,b,c){
+        //console.log(a+b+c);
+        //return a+b+c;
+       //}
+       /*sum(2,3);
+       function sum(a,b){
+        console.log(`value of a is${a} and value of is ${b}`);
+       }*/
+       //let r=sum(2,3,6)
+       //console.log(r);
+
+       //function sum(a=6,b=2,c=15){
+        //console.log(a+" "+b+" "+c);
+        //console.log(a+b+c);
+       //}
+
+       
+//function expressions
+/*let func= function(c=14,d="sakshi",e){
+    console.log('the value of c is ${c} and d:${d} and e: ${e}');
+}*/
+//func("sakshi","me","manvi");
+//func("sakshi","me","manvi");
+//let func= function(c=14,d="sakshi",e){
+  //  console.log('the value of c is ${c} and d:${d} and e: ${e}');
+//};
+//func("sakshi","me","manvi");
+//var func= function(c=14,d="sakshi",e){
+// console.log('the value of c is ${c} and d:${d} and e: ${e}');
+//}
